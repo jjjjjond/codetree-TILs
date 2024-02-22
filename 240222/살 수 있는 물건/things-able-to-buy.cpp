1f06585f8538d8649e47a41e809583ab2875f05a
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-22 11:13:15 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240222/살 수 있는 물건/things-able-to-buy.cpp
+++ 240222/살 수 있는 물건/things-able-to-buy.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n; cin >> n;
-    if (n <= 3000){
+    if (n >= 3000){
         cout << "book";
     }
     else if (1000 <= n < 3000){
