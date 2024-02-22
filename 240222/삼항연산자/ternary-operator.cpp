633e8cda94edb91bd31405625c94378e5704b724
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-22 10:37:47 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240222/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; cin >> n;
+    string res = "";
+    n == 100 ? res = "pass" : res = "failure";
+    cout << res;
+    return 0;
+}
