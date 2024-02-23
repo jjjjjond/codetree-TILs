233793f8b6fe3a4088a793c8930c1f59d8a33f28
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-23 11:52:25 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240223/범위 안의 수/number-in-range.cpp
+++ 240223/범위 안의 수/number-in-range.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a; cin >> a;
-    if (a >= 10 && 20 >= 20){
+    if (a >= 10 && 20 >= a){
         cout << "yes";
     }
     else {
