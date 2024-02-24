bba3fdb9bc72cd269daa8682aa5047ff1d2e135d
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-24 12:49:22 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240224/두 사람/two-person.cpp
+++ 240224/두 사람/two-person.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int age_1, age_2;
-    string s_1, s_2;
+    char s_1, s_2;
     cin >> age_1 >> s_1 >> age_2 >> s_2;
 
     if (age_1 >= 19 && age_2 >= 19){
-        if (s_1 == "M" || s_2 == "M"){
+        if (s_1 == 'M' || s_2 == 'M'){
             cout << "1";
         }
         else {
