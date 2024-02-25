5bc7a7914ce35e5187135c4885b5634057cfb132
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-25 23:13:18 +0900

    Title: b부터 a까지 감소, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, i; cin >> a >> b;
+    for (i = b; i >= a; i--){
+        cout << i << " ";
+    }
+    return 0;
+}
