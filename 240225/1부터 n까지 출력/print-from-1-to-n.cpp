35d1c3c55b881df0768d577d7800eea0f56da6e3
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-25 16:59:59 +0900

    Title: 1부터 n까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, i; cin >> n;
+    for (i = 1; i <= n; i++){
+        cout << i << " ";
+    }
+    return 0;
+}
