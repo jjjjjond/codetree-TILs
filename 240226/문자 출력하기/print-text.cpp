ab668cc921cd0d0985cf3feacd6fc164da84fb06
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-26 00:49:02 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/문자 출력하기/print-text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char m; cin >> m;
+    for (int i = 1; i <= 8; i++){
+        cout << m;
+    }
+    return 0;
+}
