c1e9ffb6821dab60a16e921b21ee1ecc8a19ff3a
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-26 11:08:37 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/숫자의 증감/increasing-and-decreasing-numbers.cpp
+++ 240226/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     char c; int n; cin >> c >> n;
-    if (c = 'A'){
+    if (c == 'A'){
         for (int i = 1; i <= n; i++){
             cout << i << " ";
         }
     }
-    else {
-        for (int i = 1; i <= n; i--){
+    else if (c == 'D') {
+        for (int i = n; i >= 1; i--){
             cout << i << " ";
         }
     }
