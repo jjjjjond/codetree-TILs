1642515d4dfa04c3995b7a677f94898e9ce23979
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-02-29 09:51:27 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    int res = 0;
+    cout << a / b << '.';
+    for (int i = 0; i < 20; i++){
+        a = (a % b) * 10;
+        res = a / b;
+        cout << res;
+    }
+    return 0;
+}
