98434e51f8fe5b09c6d9c5a06783ee871cd3b590
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-03-04 13:50:10 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240304/a의 b승/a-to-the-power-of-b.cpp
+++ 240304/a의 b승/a-to-the-power-of-b.cpp
@@ -5,9 +5,20 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, prod = 1; cin >> a >> b;
 
-    for (int i = a; i <= b+1; i++){
-        prod *= a;
+    if (a <= b){
+        for (int i = a; i <= b+1; i++){
+            prod *= a;
+        }
+        cout << prod;
     }
-    cout << prod;
+    
+
+    if (a > b){
+        for (int i = b; i <= a+1; i++){
+            prod *= a;
+        }
+        cout << prod;
+    }
+    
     return 0;
 }
