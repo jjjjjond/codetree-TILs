b105e98c808133bdc1e7a32ed1a02d54ec2067dd
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-03-06 19:25:31 +0900

    Title: 출석 부르기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    while(1){
+        cin >> n;
+
+        if (n == 1){
+            cout << "John" << endl;
+        }
+
+        else if (n == 2){
+            cout << "Tom" << endl;
+        }
+        
+        else if (n == 3){
+            cout << "Paul" << endl;
+        }
+
+        else if (n == 4){
+            cout << "Sam" << endl;
+        }
+
+        else {
+            cout << "Vacancy";
+            break;
+        }
+    }
+    return 0;
+}
