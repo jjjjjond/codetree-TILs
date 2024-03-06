4b9530b54b32d3f8300e4b9c68402564a94caa6f
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-03-06 19:14:46 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    while(1){
+        cin >> n;
+        if (n == 0){
+            break;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}
