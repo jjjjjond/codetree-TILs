05fb60054dbd911d5fd4195d453db7eb38042725
jjjjjond <160449088+jjjjjond@users.noreply.github.com>
2024-03-06 18:41:24 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, i, prod = 1; cin >> n;
+
+    for (i = 1; i <= 10; i++){
+        prod *= i;
+        if (prod >= n){
+            break;
+        }
+    }
+    cout << i;
+    return 0;
+}
